f8276928d6f7900e849d002f433798d2e9538d32
Uluc Kaymak <44571005+UlucKaymak@users.noreply.github.com>
2025-07-10 02:11:25 +0300

    Update TwitterAPI.py for Privacy

--- functions/TwitterAPI.py
+++ functions/TwitterAPI.py
@@ -3,19 +3,19 @@ from termcolor import colored
 import tweepy
 import json
 
-API_KEY = 'bJmDaKCfQRRpH9lTFLumA1ObK'
-API_SECRET_KEY = 'ubt4pMXMUiB9w3eyCcf30Tusqvg9qBYJC7s1JZaYsF0ZtL7clT'
-BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAC%2F8vQEAAAAArk%2BALPRsYcKSzU6bEBqUav6HQag%3DpqVWH7fDph5KGT08ZKQsTXOeRkiHDhjkNWv7OS2zXKFHdPHSCh'
-ACCESS_TOKEN = '1826593040430940161-nml90CwWrVCFaMwkxvKL5ltYr5pFb0'
-ACCESS_TOKEN_SECRET = 'qdeHvAZ5IBCkLPuVsw6qaBqGocIy4yKY5c748YB5cFFUG'
+API_KEY = '-'
+API_SECRET_KEY = '-'
+BEARER_TOKEN = '-'
+ACCESS_TOKEN = '-'
+ACCESS_TOKEN_SECRET = '-'
 
-    # Initialize Tweepy v2 Client for creating the tweet
+# Initialize Tweepy v2
 client = tweepy.Client(consumer_key=API_KEY,    
                     consumer_secret=API_SECRET_KEY,
                     access_token=ACCESS_TOKEN,
                     access_token_secret=ACCESS_TOKEN_SECRET)
     
-    # Initialize Tweepy v1.1 API for media upload
+# Initialize Tweepy v1.1
 auth = tweepy.OAuth1UserHandler(consumer_key=API_KEY, 
                                 consumer_secret=API_SECRET_KEY, 
                                 access_token=ACCESS_TOKEN, 
@@ -23,7 +23,6 @@ auth = tweepy.OAuth1UserHandler(consumer_key=API_KEY,
 api = tweepy.API(auth)
 
 def post_tweet():
-    # Read data from current_data.json file
     with open(current_data, "r") as file:
         data_artwork = json.load(file)
 
@@ -38,10 +37,9 @@ def post_tweet():
     # print(tweet_text)
 
     try:
-        # Upload image using Tweepy v1.1 API
         media = api.media_upload(postdata_image)
 
-        # Create the tweet with media attached using Tweepy v2 Client
+        # Tweepy v2 Client
         response = client.create_tweet(text=tweet_text, media_ids=[media.media_id])
         tweet_url = f"https://twitter.com/{ACCESS_TOKEN.split('-')[0]}/status/{response.data['id']}"
         print(colored("Tweet successfully posted!", "green"))
